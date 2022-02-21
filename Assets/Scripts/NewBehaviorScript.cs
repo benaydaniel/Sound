0afe7a78b799920dfa7ed1df7220deabec54ba72
Benay Daniel <benayjdaniel@gmail.com>
2022-02-20 23:33:57 -0500

    Animation commit

--- Assets/Scripts/NewBehaviorScript.cs
+++ Assets/Scripts/NewBehaviorScript.cs
@@ -8,23 +8,32 @@ public class NewBehaviorScript : MonoBehaviour
     public AudioClip musicClipOne;
     public AudioClip musicClipTwo;
     // Start is called before the first frame update
+    Animator anim;
     void Start()
     {
-        
+         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
-    {if (Input.GetKeyDown(KeyCode.W))
+    {if (Input.GetKey("escape"))
+      {
+        Application.Quit();
+      }
+
+      
+      if (Input.GetKeyDown(KeyCode.W))
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
+          anim.SetInteger("State", 1);
 
          }
 
      if (Input.GetKeyUp(KeyCode.W))
         {
           musicSource.Stop();
+           anim.SetInteger("State", 0);
 
          }
 
@@ -32,11 +41,13 @@ public class NewBehaviorScript : MonoBehaviour
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
+          anim.SetInteger("State", 2);
          }
 
      if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
+          anim.SetInteger("State", 0);
 
          }
 
